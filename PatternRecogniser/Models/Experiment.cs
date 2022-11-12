b4f28962c529cf97aab9481e470ba4e59bbe7e7f
Evva28 <93273156+Evva28@users.noreply.github.com>
2022-11-12 13:43:45 +0100

    stworzenie szablonów klas i implementacja najprostszych funkcji (klasa Pattern i AddExperimentw User)

---
+++ PatternRecogniser/Models/Experiment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Drawing;
+
+namespace PatternRecogniser.Models
+{
+    public abstract class Experiment
+    {
+        public ExtendedModel usedModel { get; set; }
+
+        public string GetResults()
+        {
+            return ""; // returns a string that follows json formatting
+        }
+
+        public void SaveResult() { }
+    }
+
+    public class ExperimentList
+    {
+        public string name { get; set; }
+        public List<Experiment> experiments;
+    }
+
+    public class ModelTrainingExperiment : Experiment
+    {
+        public List<(Bitmap, Pattern, Pattern)> validationSet;
+        public double accuracy { get; set; }
+        public double precision { get; set; }
+        public double recall { get; set; }
+        public double specificity { get; set; }
+        public double missRate { get; set; }
+        public int[,] confusionMatrix { get; set; }
+        private int TP;
+        private int TN;
+        private int FP;
+        private int FN;
+    }
+
+    public class PatternRecognitionExperiment : Experiment
+    {
+        public Bitmap testedPattern { get; set; }
+        public List<(Pattern, double)> recognisedPatterns;
+    }
+}
