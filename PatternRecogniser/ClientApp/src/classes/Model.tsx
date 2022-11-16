1fb62a3c1e6fa8af3cb5b4aa15b4bbecd86a94e0
Aizos <91993691+Aizos@users.noreply.github.com>
2022-11-16 19:16:44 +0100

    Dodano klasę elementów listy modeli

---
+++ PatternRecogniser/ClientApp/src/classes/Model.tsx
@@ -0,0 +1,5 @@
+export interface Model {
+    name: string,
+    patternNum: number
+}
+
--- PatternRecogniser/ClientApp/src/pages/MyModels.tsx
+++ PatternRecogniser/ClientApp/src/pages/MyModels.tsx
@@ -1,14 +1,31 @@
-import {Typography, Form, Card, Space, Select, Tooltip, UploadProps } from "antd"
+import {Typography, Form, Card, Space, Select, Tooltip, UploadProps, Input, Button } from "antd"
 
 import 'antd/dist/antd.min.css';
 import { Row, Col, message, Upload } from "antd";
 import { useState } from "react";
-import { QuestionCircleOutlined } from '@ant-design/icons';
+import { QuestionCircleOutlined, SearchOutlined } from '@ant-design/icons';
+import { Model } from "../classes/Model";
+import ModelListElement from "./ModelListElement";
 
 const { Title } = Typography;
 
 const MyModelsPage = () => {
     const [form] = Form.useForm();
+
+    const exampleModels : Model[] = [
+        {
+            name: "Cyfry arabskie",
+            patternNum: 10
+        },
+        {
+            name: "Alfabet",
+            patternNum: 32
+        },
+        {
+            name: "Figury geometryczne",
+            patternNum: 56
+        }
+    ]
 
     return (
         <div>
@@ -19,7 +36,24 @@ const MyModelsPage = () => {
                             <Title>Moje modele</Title>
                         </Row>
 
-                        Treść
+                        <Row justify="space-around" align="middle">
+                            <Row justify="space-between" align="middle" style={{width: "150vh", marginBottom: '20px'}}>
+                                <Col>
+                                    <Input placeholder="Wyszukaj" prefix={<SearchOutlined />} />
+                                </Col>
+                                <Col>
+                                    <Button type="default">Dodaj nowy model</Button>
+                                </Col>
+                            </Row>
+
+                            <Row justify="space-around" align="middle">
+                                <Card bordered={true} style={{width: "150vh"}}>
+                                    {
+                                        exampleModels.map((item: Model) => (<ModelListElement model={item}/> ))
+                                    }
+                                </Card>
+                            </Row>
+                        </Row>
                     </div>      
                 </Col>
             </Row>
