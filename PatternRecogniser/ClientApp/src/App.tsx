ade8e862ea7f314f1424b3c59a8e5510ce69109a
Aizos <91993691+Aizos@users.noreply.github.com>
2022-11-17 18:21:39 +0100

    Revert "Wylogowywanie po wyłączeniu okna"
    This reverts commit cbb83e2e51da25cc206b49418ac11c2eca321e62.

--- PatternRecogniser/ClientApp/src/App.tsx
+++ PatternRecogniser/ClientApp/src/App.tsx
@@ -3,26 +3,17 @@ import { AppLayout } from './components/AppLayout';
 import { AppRouter } from './components/AppRouter'
 import './custom.css'
 import { GlobalStore } from './reducers/GlobalStore';
-import { Beforeunload } from 'react-beforeunload';
-
-const removeApplicationData = () => {
-    if (window) {
-        localStorage.clear();
-    }
-};
 
 export default class App extends Component {
     static displayName = App.name;
   
     render () {
         return (
-            <Beforeunload onBeforeunload={removeApplicationData}>
-                <GlobalStore>
-                    <AppLayout>
-                        <AppRouter />
-                    </AppLayout>
-                </GlobalStore>
-            </Beforeunload>
+            <GlobalStore>
+                <AppLayout>
+                    <AppRouter />
+                </AppLayout>
+            </GlobalStore>
         );
     }
 }
