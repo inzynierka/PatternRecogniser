cbb83e2e51da25cc206b49418ac11c2eca321e62
Aizos <91993691+Aizos@users.noreply.github.com>
2022-11-17 18:17:40 +0100

    Wylogowywanie po wyłączeniu okna

--- PatternRecogniser/ClientApp/src/App.tsx
+++ PatternRecogniser/ClientApp/src/App.tsx
@@ -3,17 +3,26 @@ import { AppLayout } from './components/AppLayout';
 import { AppRouter } from './components/AppRouter'
 import './custom.css'
 import { GlobalStore } from './reducers/GlobalStore';
+import { Beforeunload } from 'react-beforeunload';
+
+const removeApplicationData = () => {
+    if (window) {
+        localStorage.clear();
+    }
+};
 
 export default class App extends Component {
     static displayName = App.name;
   
     render () {
         return (
-            <GlobalStore>
-                <AppLayout>
-                    <AppRouter />
-                </AppLayout>
-            </GlobalStore>
+            <Beforeunload onBeforeunload={removeApplicationData}>
+                <GlobalStore>
+                    <AppLayout>
+                        <AppRouter />
+                    </AppLayout>
+                </GlobalStore>
+            </Beforeunload>
         );
     }
 }
