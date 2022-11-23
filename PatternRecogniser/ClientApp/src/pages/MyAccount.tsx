87db9bea948fcd47384929db8271d2f5f5e630a0
Aizos <91993691+Aizos@users.noreply.github.com>
2022-11-24 00:54:10 +0100

    MyAccount layout fix

--- PatternRecogniser/ClientApp/src/pages/MyAccount.tsx
+++ PatternRecogniser/ClientApp/src/pages/MyAccount.tsx
@@ -22,7 +22,15 @@ const MyAccountPage = () => {
 
                         <Row justify="space-around" align="middle">
                             <Card bordered={true} style={{width: isOrientationVertical ? "30vw" : "50vw", boxShadow: '0 3px 10px rgb(0 0 0 / 0.2)' }}>
-                                <Row justify="center" align="middle">
+                                <Row justify="center" align="middle" style={{
+                                    marginTop: "35px", 
+                                    marginBottom: "35px", 
+                                    backgroundColor: 'rgba(0, 0, 0, 0.01)', 
+                                    paddingBlock: '25px', 
+                                    paddingBottom: '10px', 
+                                    marginRight: '35px', 
+                                    marginLeft: '35px'
+                                }}>
                                     <Col>
                                         <Row justify="space-around" align="middle" style={{marginRight: "35px"}}>
                                             <p style={{fontSize: "1.5em"}}>Adres e-mail:</p>
