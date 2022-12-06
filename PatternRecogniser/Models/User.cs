af2480f2ed68f78af498c2dbe6887ea605703226
Evva28 <93273156+Evva28@users.noreply.github.com>
2022-12-06 12:55:18 +0100

    zaimplementowana binaryzacja

--- PatternRecogniser/Models/User.cs
+++ PatternRecogniser/Models/User.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -112,13 +113,16 @@ namespace PatternRecogniser.Models
             }
         }
 
-        public void LoadTestingImage() 
-        { 
+        public void LoadTestingImage(string path) 
+        {
             // też jakoś w kontrolerze
             // na froncie otwieramy file explorer i wybieramy plik
             // przekazujemy tutaj ścieżkę do pliku
-            // Bitmap img = OpenSingleImage(path);
-            // wywołujemy RecognisePattern z wybranego ExtendedModel
+
+            Bitmap img = OpenSingleImage (path);
+            int[,] matrix = NormaliseData (img);
+
+            // matrix możemy przekazać do RecognisePattern w ExtendedModel
         }
 
         private Bitmap OpenSingleImage(string path)
@@ -146,14 +150,97 @@ namespace PatternRecogniser.Models
             }
         }
 
-        private int[] NormaliseData ()
+        private int[,] NormaliseData (Bitmap bmp)
         {
             // jeden obrazek Bitmap
-            // sprowadzamy do skali szarości
-            // zwiększamy kontrast?
-            // zaokrąglamy do wartości 0 i 1
-            // robimy macierz
-            return null;
+            bmp = new Bitmap (bmp, 28, 28);
+
+            int height = bmp.Height; // 28
+            int width = bmp.Width; // 28
+            Bitmap contrastBmp = AdjustContrast (bmp, 100.0f);
+
+
+            int[,] binaryMatrix = new int[height, width];
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    // sprowadzamy kolor do skali szarości
+                    Color pixelColor = contrastBmp.GetPixel (i, j);
+                    int avg = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+
+                    // odnaleziony kolor sprowadzamy do wartości 0/1
+                    // normalnie 0 - czarny, 255 - biały
+                    // zatem dzielimy przez 255 i zamieniamy
+                    // wpisujemy do pola [j, i] żeby zapobiec obróceniu obrazka
+                    float tmp = avg / 255;
+                    if (tmp < 0.5) // czarny
+                    {
+                        binaryMatrix[j, i] = 1;
+                    }
+                    else // biały
+                    {
+                        binaryMatrix[j, i] = 0;
+                    }
+                }
+            }
+
+            return binaryMatrix;
+        }
+
+        private static Bitmap AdjustContrast (Bitmap Image, float Value) // https://stackoverflow.com/questions/3115076/adjust-the-contrast-of-an-image-in-c-sharp-efficiently
+        {
+            Value = (100.0f + Value) / 100.0f;
+            Value *= Value;
+            Bitmap NewBitmap = (Bitmap)Image.Clone ();
+            BitmapData data = NewBitmap.LockBits (
+                new Rectangle (0, 0, NewBitmap.Width, NewBitmap.Height),
+                ImageLockMode.ReadWrite,
+                NewBitmap.PixelFormat);
+            int Height = NewBitmap.Height;
+            int Width = NewBitmap.Width;
+
+            unsafe // włączone "allow unsafe code"!!!
+            {
+                for (int y = 0; y < Height; ++y)
+                {
+                    byte* row = (byte*)data.Scan0 + (y * data.Stride);
+                    int columnOffset = 0;
+                    for (int x = 0; x < Width; ++x)
+                    {
+                        byte B = row[columnOffset];
+                        byte G = row[columnOffset + 1];
+                        byte R = row[columnOffset + 2];
+
+                        float Red = R / 255.0f;
+                        float Green = G / 255.0f;
+                        float Blue = B / 255.0f;
+                        Red = (((Red - 0.5f) * Value) + 0.5f) * 255.0f;
+                        Green = (((Green - 0.5f) * Value) + 0.5f) * 255.0f;
+                        Blue = (((Blue - 0.5f) * Value) + 0.5f) * 255.0f;
+
+                        int iR = (int)Red;
+                        iR = iR > 255 ? 255 : iR;
+                        iR = iR < 0 ? 0 : iR;
+                        int iG = (int)Green;
+                        iG = iG > 255 ? 255 : iG;
+                        iG = iG < 0 ? 0 : iG;
+                        int iB = (int)Blue;
+                        iB = iB > 255 ? 255 : iB;
+                        iB = iB < 0 ? 0 : iB;
+
+                        row[columnOffset] = (byte)iB;
+                        row[columnOffset + 1] = (byte)iG;
+                        row[columnOffset + 2] = (byte)iR;
+
+                        columnOffset += 4;
+                    }
+                }
+            }
+
+            NewBitmap.UnlockBits (data);
+
+            return NewBitmap;
         }
 
         public void SaveResult(Experiment experiment) { }
