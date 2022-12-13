458e2a009e990f7a5ac4b92adb02f6805aa2d811
Aizos <91993691+Aizos@users.noreply.github.com>
2022-12-14 00:51:15 +0100

    Próba nawiązania połączenia z bakcendem

--- PatternRecogniser/ClientApp/src/generated/ApiServiceConfig.ts
+++ PatternRecogniser/ClientApp/src/generated/ApiServiceConfig.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://localhost:44314" //"https://patternrecogniser-apim.azure-api.net"
+export const BASE_URL = "https://patternrecogniser-apim.azure-api.net" //"https://patternrecogniser.azurewebsites.net" //"https://localhost:44314"
