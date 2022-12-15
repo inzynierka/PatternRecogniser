3d16abeb17264b2b6093fe71931621a26905d395
CoBskUra <84574377+CoBskUra@users.noreply.github.com>
2022-12-15 01:29:07 +0100

    testy, postępy

---
+++ PatternRecogniser/Services/NewFolder/AuthenticationRepository.cs
@@ -0,0 +1,26 @@
+﻿//using PatternRecogniser.Models;
+//using System.Collections;
+//using System.Collections.Generic;
+
+//namespace PatternRecogniser.Services.NewFolder
+//{
+//    public interface IAuthenticationRepo
+//    {
+//        public IEnumerator<User> GetUsers();
+
+//    public class AuthenticationRepository: IAuthenticationRepo
+//        {
+
+//        private PatternRecogniserDBContext _context;
+
+//        public AuthenticationRepository(PatternRecogniserDBContext context)
+//        { 
+//            _context = context;
+//        }
+
+//        //    public IEnumerator<User> GetUsers()
+//        //    {
+//        //        return _context.user;
+//        //    }
+//        //}
+//}
--- PatternRecogniserUnitTests/AuthenticationUnitTests.cs
+++ PatternRecogniserUnitTests/AuthenticationUnitTests.cs
@@ -36,6 +36,7 @@ namespace PatternRecogniserUnitTests
             };
         }
 
+        [TestMethod]
         public void InputDataValidatorTest()
         {
             _mockRepo.Setup(repo => repo.IsLoginTaken(signUpInfo.login))
