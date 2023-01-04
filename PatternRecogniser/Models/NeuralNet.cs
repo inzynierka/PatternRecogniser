d2a152eb9e07d948288e52a9eb96ea76c3cf1a6d
Evva28 <93273156+Evva28@users.noreply.github.com>
2023-01-04 18:16:19 +0100

    dodanie naszego layera do modelu rip accuracy

--- PatternRecogniser/Models/NeuralNet.cs
+++ PatternRecogniser/Models/NeuralNet.cs
@@ -12,6 +12,7 @@
 ******************************************************************************/
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Tensorflow;
 using Tensorflow.Keras;
@@ -37,6 +38,15 @@ namespace PatternRecogniser.Models
     {
         ILayer fc1;
         ILayer fc2;
+        ILayer fc3;
+        ILayer fc4;
+        ILayer fc5;
+        ILayer fc6;
+        ILayer fc7;
+        ILayer fc8;
+        ILayer fc9;
+        ILayer fc10;
+        ILayer fc11;
         ILayer output;
 
         public NeuralNet (NeuralNetArgs args) :
@@ -45,14 +55,27 @@ namespace PatternRecogniser.Models
             var layers = keras.layers;
 
             // First fully-connected hidden layer.
-            fc1 = layers.Dense (args.NeuronOfHidden1, activation: args.Activation1);
+            //fc1 = layers.Dense (args.NeuronOfHidden1, activation: args.Activation1);
 
             // Second fully-connected hidden layer.
-            fc2 = layers.Dense (args.NeuronOfHidden2, activation: args.Activation2);
+            //fc2 = layers.Dense (args.NeuronOfHidden2, activation: args.Activation2);
+
+            // nasze warstwy
+            fc1 = OurLayer (26 * 26, activation: args.Activation1);
+            fc2 = OurLayer (24 * 24, activation: args.Activation2);
+            fc3 = OurLayer (22 * 22, activation: args.Activation2);
+            fc4 = OurLayer (20 * 20, activation: args.Activation2);
+            fc5 = OurLayer (18 * 18, activation: args.Activation2);
+            fc6 = OurLayer (16 * 16, activation: args.Activation2);
+            fc7 = OurLayer (14 * 14, activation: args.Activation2);
+            fc8 = OurLayer (12 * 12, activation: args.Activation2);
+            fc9 = OurLayer (10 * 10, activation: args.Activation2);
+            fc10 = OurLayer (8 * 8, activation: args.Activation2);
+            fc11 = OurLayer (6 * 6, activation: args.Activation2);
 
             output = layers.Dense (args.NumClasses);
 
-            StackLayers (fc1, fc2, output);
+            StackLayers (fc1, fc2, fc3, fc4, fc5, fc6, fc7, fc8, fc9, fc10, fc11, output);
         }
 
         // Set forward pass.
@@ -60,10 +83,31 @@ namespace PatternRecogniser.Models
         {
             inputs = fc1.Apply (inputs);
             inputs = fc2.Apply (inputs);
+            inputs = fc3.Apply (inputs);
+            inputs = fc4.Apply (inputs);
+            inputs = fc5.Apply (inputs);
+            inputs = fc6.Apply (inputs);
+            inputs = fc7.Apply (inputs);
+            inputs = fc8.Apply (inputs);
+            inputs = fc9.Apply (inputs);
+            inputs = fc10.Apply (inputs);
+            inputs = fc11.Apply (inputs);
             inputs = output.Apply (inputs);
             if (!training.Value)
                 inputs = tf.nn.softmax (inputs);
             return inputs;
+        }
+
+        private OurLayer OurLayer (int units, Activation activation = null, IInitializer kernel_initializer = null, bool use_bias = true, IInitializer bias_initializer = null, Shape input_shape = null)
+        {
+            return new OurLayer (new OurLayerArgs
+            {
+                Units = units,
+                Activation = (activation ?? KerasApi.keras.activations.Linear),
+                KernelInitializer = (kernel_initializer ?? Binding.tf.glorot_uniform_initializer),
+                BiasInitializer = (bias_initializer ?? (use_bias ? Binding.tf.zeros_initializer : null)),
+                InputShape = input_shape
+            });
         }
     }
 
