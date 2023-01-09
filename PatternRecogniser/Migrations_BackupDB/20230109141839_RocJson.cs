8784accd9c464b9f721cd02c2c134202f8755eec
CoBskUra <84574377+CoBskUra@users.noreply.github.com>
2023-01-09 15:22:06 +0100

    data update

---
+++ PatternRecogniser/Migrations_BackupDB/20230109141839_RocJson.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PatternRecogniser.Migrations_BackupDB
+{
+    public partial class RocJson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "serializedRoc",
+                table: "ModelTrainingExperiment",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "serializedRoc",
+                table: "ModelTrainingExperiment");
+        }
+    }
+}
