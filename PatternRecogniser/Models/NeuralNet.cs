79de48b04d85b26f6fe4b3166a8494f6e48e36e7
Evva28 <93273156+Evva28@users.noreply.github.com>
2023-01-21 20:45:28 +0100

    usunięcie komentarzy

--- PatternRecogniser/Models/NeuralNet.cs
+++ PatternRecogniser/Models/NeuralNet.cs
@@ -48,9 +48,7 @@ namespace PatternRecogniser.Models
         ILayer fc10;
         ILayer fc11;
         ILayer fc12;
-        ILayer fc13;
 
-        ILayer dense;
         ILayer output;
 
         public AutomataModel (AutomataModelArgs args) :
@@ -153,14 +151,6 @@ namespace PatternRecogniser.Models
                 { 0, 0, 0, 0, 1 },
                 { 1, 1, 1, 1, 1 }
             };
-            //float[,] rule13 = new float[5, 5]
-            //{
-            //    { 0, 0, 1, 0, 0 },
-            //    { 0, 0, 1, 0, 0 },
-            //    { 1, 1, 1, 1, 1 },
-            //    { 0, 0, 1, 0, 0 },
-            //    { 0, 0, 1, 0, 0 }
-            //};
 
             fc1 = AutomataConvLayer (24 * 24, rule1, 5, activation: args.Activation);
             fc2 = AutomataConvLayer (24 * 24, rule2, 5, activation: args.Activation);
@@ -174,9 +164,7 @@ namespace PatternRecogniser.Models
             fc10 = AutomataConvLayer (24 * 24, rule10, 8, activation: args.Activation);
             fc11 = AutomataConvLayer (24 * 24, rule11, 8, activation: args.Activation);
             fc12 = AutomataConvLayer (24 * 24, rule12, 8, activation: args.Activation);
-            //fc13 = AutomataConvLayer (24 * 24, rule13, 8, activation: args.Activation);
 
-            //dense = layers.Dense (20);
             output = layers.Dense (args.NumClasses, activation: "softmax");
 
             StackLayers (fc1, fc2, fc3, fc4, fc5, fc6, fc7, fc8, fc9, fc10, fc11, fc12, output);
@@ -184,7 +172,6 @@ namespace PatternRecogniser.Models
 
         protected override Tensors Call (Tensors inputs, Tensor state = null, bool? training = null)
         {
-            //inputs = fc1.Apply (inputs);
             Tensors result1 = fc1.Apply (inputs);
             Tensors result2 = fc2.Apply (inputs);
             Tensors result3 = fc3.Apply (inputs);
@@ -197,11 +184,9 @@ namespace PatternRecogniser.Models
             Tensors result10 = fc10.Apply (inputs);
             Tensors result11 = fc11.Apply (inputs);
             Tensors result12 = fc12.Apply (inputs);
-            //Tensors result13 = fc13.Apply (inputs);
 
             inputs = JoinResults (new List<Tensors> { result1, result2, result3, result4, result5, result6, result7, result8, result9, result10, result11, result12});
 
-            //inputs = dense.Apply (inputs);
             inputs = output.Apply (inputs);
 
             if (!training.Value)
@@ -244,51 +229,6 @@ namespace PatternRecogniser.Models
                 outputs.Add (ops.convert_to_tensor (newResults.ToArray ()));
             }
 
-
-            return outputs;
-        }
-
-        private Tensors JoinResults (Tensors result1, Tensors result2)
-        {
-            int count = result1.Count ();
-            // result2.Count() powinno być równe
-
-            Tensors outputs = new Tensors ();
-            for (int i = 0; i < count; i++)
-            {
-                List<float[]> allResults1 = new List<float[]> ();
-                foreach (var np in result1[i].numpy ())
-                {
-                    float[] arr = np.ToArray<float> ();
-                    allResults1.Add (arr);
-                }
-
-                List<float[]> allResults2 = new List<float[]> ();
-                foreach (var np in result2[i].numpy ())
-                {
-                    float[] arr = np.ToArray<float> ();
-                    allResults2.Add (arr);
-                }
-
-                // łączymy
-                List<float[]> allResults = new List<float[]> ();
-                for (int j = 0; j < allResults1.Count; j++)
-                {
-                    float[] newArr = new float[allResults1[j].Length];
-
-                    for (int k = 0; k < allResults1[j].Length; k++)
-                    {
-                        if (allResults1[j][k] == 1 || allResults2[j][k] == 1)
-                            newArr[k] = 1;
-                        else
-                            newArr[k] = 0;
-                    }
-
-                    allResults.Add (newArr);
-                }
-
-                outputs.Add (ops.convert_to_tensor (allResults.ToArray ()));
-            }
 
             return outputs;
         }
